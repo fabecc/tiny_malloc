97687d241bb3fdfe887262b2dd23c575028bcf69
Fabrice Hesling <fabrice.hesling>
2020-06-08 09:40:50 +0200

    Add tm implementation skel

---
+++ tm.c
@@ -0,0 +1,26 @@
+
+#include "tm.h"
+
+// Init Tiny Malloc
+int tm_init(char* mem, int mem_size, int block_size)
+{
+
+}
+
+// Get Memory
+void* tm_malloc(void)
+{
+
+}
+
+// Free previous given memory block
+void tm_free(void* data)
+{
+
+}
+
+// Print stat on memory usage
+void tm_stats(void)
+{
+
+}
