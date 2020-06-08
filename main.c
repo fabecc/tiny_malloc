826caa6107e6c143a8cf92b7078e1f6166da4e32
Fabrice Hesling <fabrice.hesling>
2020-06-08 10:20:31 +0200

    Imprive main message

--- main.c
+++ main.c
@@ -2,23 +2,35 @@
 #include <stdio.h>
 #include "tm.h"
 
+void perr(char* msg)
+{
+	printf("Error: %s\n", msg);
+}
+
 int main(void)
 {
 	int res;
 
 	// Init tiny malloc module
 	res = tm_init();
-	if (res != 0)
-		perror("Failed to init tyny malloc");
+	if (res != 0) {
+		perr("Failed to init tyny malloc");
+		return 1;
+	}
 
 	// Print stats
 	tm_stats();
 
 	// Get memory
-	void* data = tm_malloc();
-	if (data == NULL)
-		perror("Failed to get memory");
-	printf("Get memory address %p", data);
+	int* data = (int*)tm_malloc();
+	if (data == NULL) {
+		perr("Failed to get memory");
+		return 1;
+	}
+	printf("Get memory address %p\n", (void*)data);
+
+	// Try to write something in memory
+	*data = 0xdeadbeaf;
 
 	// Free memory
 	tm_free(data);
